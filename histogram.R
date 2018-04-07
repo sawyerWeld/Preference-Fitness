e7eb584ce3049933ecd9a34c12ffa535a7966b88
SawyerWelden <sawyer.welden15@ncf.edu>
2018-04-07 18:34:52 -0400

    renamed X axis

--- histogram.R
+++ histogram.R
@@ -9,4 +9,4 @@ for (i in 1:dim(data)[2]) {
   Dispersion[,i] <- c(as.numeric(data[[i]]))
 }
 
-hist(dataNum, breaks = 200, main = "Parameter Estimate")
+hist(Dispersion, breaks = 200, main = "Parameter Estimate")
