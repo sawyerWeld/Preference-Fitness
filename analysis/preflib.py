c4611eae7905e54ea4e8a7d5b3e489faa5eb1e23
Sawyer Welden <sawyer.welden@gmail.com>
2018-09-30 16:33:18 -0400

    Read in preflib-style Strict Order Incomplete data

--- analysis/preflib.py
+++
@@ -1,7 +0,0 @@
-# Reading in data from the preflib format
-with open('EDIreland.soi', 'r') as file:
-    data = file.readlines()
-    for line in data:
-        print(line)
-            
-print('Finished writing to file')
