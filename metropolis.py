ccbcfde6efadfcbec09829f307bb9543c8170c69
Sawyer Welden <sawyer.welden@gmail.com>
2018-11-08 06:39:24 -0500

    Working on small N-runs, going to let it run while sleeping

--- metropolis.py
+++ metropolis.py
@@ -2,15 +2,15 @@ import random
 import numpy as np
 from tqdm import tqdm_notebook
 
-def maximize(cost_model, params, gen_candidate, dataset, runs):
-    initial_cost = cost_model(params, dataset)
+def maximize(cost_model, lengths, params, gen_candidate, dataset, runs):
+    initial_cost = cost_model(params, dataset, lengths)
     greatest_cost = initial_cost
     max_params = None
     
-    for step in tqdm_notebook(range(runs)):
+    for step in tqdm_notebook(range(runs), desc = cost_model.__name__):
         new_params = gen_candidate(params)
-        prev_cost = cost_model(params, dataset)
-        new_cost = cost_model(params, dataset)
+        prev_cost = cost_model(params, dataset, lengths)
+        new_cost = cost_model(params, dataset, lengths)
         
         u = np.random.uniform(0,1)
         alpha = (1.0 * new_cost) / prev_cost
