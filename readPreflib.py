505d9ce473b08504ab9b723433e7916ac7edc084
Sawyer Welden <sawyer.welden@gmail.com>
2018-11-14 00:37:40 -0500

    Sum of squares divergence programmed

--- readPreflib.py
+++ readPreflib.py
@@ -58,3 +58,33 @@ def soiInputwithWeights(filename):
 
 
     return candidates, length_counts, votes
+
+def soiInputwithNumVotes(filename):
+
+    candidates = {}
+
+    votes = []
+
+    length_counts = defaultdict(int)
+
+    with open(filename) as file:
+        data = file.readlines()
+        num_alternatives = int(data[0])
+        num_votes = int(data[num_alternatives+1].split(',')[0])
+
+        # Make Candidate Names Dict
+        for i in range(1, num_alternatives+1):
+            string = data[i]
+            split_pos = string.find(',') + 1
+            candidates[i] = string[split_pos:-2]
+
+        # Wrangel Orderings
+        for i in range(num_alternatives+2, len(data)):
+            row = data[i].split(',')
+            num_occurances = int(row[0])
+            nums = list(map(int, row[1:]))
+            length_counts[len(nums)] += num_occurances
+            votes.append((num_occurances, nums))
+
+
+    return candidates, length_counts, num_votes, votes
